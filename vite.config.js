269f307d1cfb85e128f165c3b250e7b2d84884fe
Aureo-Bueno <aureoalexandre@outlook.com>
2024-09-17 20:29:15 -0300

    feat: add new package plugin pwa vite and config pwa vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,51 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        id: "/",
+        name: "my-app",
+        short_name: "my-app",
+        theme_color: "#5a189a",
+        background_color: "#09001a",
+        display: "standalone",
+        description: "My awesome app",
+        orientation: "portrait",
+        start_url: "/",
+        icons: [
+          {
+            src: "/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/icon-256x256.png",
+            sizes: "256x256",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+      devOptions: { enabled: true },
+    }),
+  ],
+});
